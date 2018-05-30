11645d7ec31d51de109baf5704ae8fb6c40a4b84
ktp <t08265hk@gmail.com>
2018-05-30 21:24:02 +0900

    first commit.

---
+++ app/controllers/webhook_controller.rb
@@ -0,0 +1,42 @@
+require 'sinatra'
+require 'line/bot'
+
+class WebhookController < ApplicationController
+  protect_from_forgery with: :null_session # CSRF対策無効化
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
+
+  def callback
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      head 470
+    end
+
+    events = client.parse_events_from(body)
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
+        end
+      end
+    }
+    head :ok
+  end
+end
