f04c025ec41cc6ef49b7bc6256f43e2ac6ec92cb
Kazuki Yamagata <zsp2088dev@gmail.com>
2019-09-04 15:59:22 +0900

    Use Cafe with WiFi List instead of query parameter

--- app/lib/gnavi_request.rb
+++ app/lib/gnavi_request.rb
@@ -6,7 +6,7 @@ require 'json'
 GNAVI_API_KEY = ENV['GNAVI_API_KEY']
 GNAVI_REST_SEARCH_BASE_URL = 'https://api.gnavi.co.jp/RestSearchAPI/v3'
 
-class GnaviRestSearchAPI
+class GnaviRequest
   def gnavi_rest_search(latitude, longitude)
     url = '%s/?keyid=%s&category_l=RSFST18000&range=2&latitude=%s&longitude=%s&hit_per_page=50' % [GNAVI_REST_SEARCH_BASE_URL, GNAVI_API_KEY, latitude, longitude]
     response = OpenURI.open_uri(url).read
@@ -14,8 +14,33 @@ class GnaviRestSearchAPI
   end
 
   def response_processing(hash)
-    hash['rest'][0]['name']
+    messages = []
+    hash['rest'].each { |cafe|
+      if compare_cafe(cafe['name_kana'], cafe_with_wifi_list)
+        messages.push(cafe)
+      end
+    }
+    messages[0]['name']
+
   end
+
+  # cafe_with_wifi_listに含まれるカフェかどうかをチェック
+  def compare_cafe(name_kana, list)
+    result = false
+    list.each { |cafe|
+      if name_kana.include?(cafe)
+        result = true
+        break
+      end
+    }
+    result
+  end
+
+  # Level2ではWiFiの使えるカフェをここに追加
+  def cafe_with_wifi_list
+    %w(スターバックスコーヒー ドトール プロント タリーズ マクドナルド ウエシマコーヒー ロッテリア フレッシュネス コメダコーヒー)
+  end
+
 
   def cafe_with_wifi(latitude, longitude)
     response_processing(gnavi_rest_search(latitude, longitude))
